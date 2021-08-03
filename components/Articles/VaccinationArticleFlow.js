0882c8a131d900c2dd8953eb5032c20af5100537
tylerdgenius <87417020+tylerdgenius@users.noreply.github.com>
2021-08-03 06:05:03 -0700

    Partial Login System

---
+++ components/Articles/VaccinationArticleFlow.js
@@ -0,0 +1,8 @@
+const VaccinationArticleFlow = () => {
+    return (
+        <View>
+            <Text>Vaccination Article</Text>
+        </View>
+    )}
+
+    export default VaccinationArticleFlow;
